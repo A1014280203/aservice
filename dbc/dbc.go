d05080a8bf00a50e760de0723aab2a1d0a2baf20
A1014280203 <1014280203@qq.com>
2017-11-15 20:25:24 +0800

    修改redis

--- dbc/dbc.go
+++ dbc/dbc.go
@@ -15,10 +15,11 @@ var conn redis.Conn
 var err error
 
 func init() {
-	db, err = sql.Open("mysql", "")
+	db, err = sql.Open("mysql", "root:1014280203qq@tcp(115.159.34.131:3306)/goweb?charset=utf8")
 	checkErr(err)
-	conn, err = redis.Dial("", "")
+	conn, err = redis.Dial("tcp", "123.206.214.238:6379")
 	checkErr(err)
+	// 检查pong?
 }
 
 func checkErr(err error) {
@@ -60,10 +61,10 @@ func AppendUser(phone, password, nickname string) error {
 	_, err = stmt.Exec(phone, genHashPassword(phone, password), nickname)
 	if err != nil {
 		// log 自带时间部分
-		log.Printf("Cannot append user[%s %s], for %s\n\r", phone, password, nickname, err.Error())
+		log.Printf("Cannot append user[%s %s], for %s\n", phone, password, nickname, err.Error())
 		return err
 	}
-	log.Printf("Append user[%s]\n\r", phone)
+	log.Printf("Append user[%s]\n", phone)
 	return nil
 }
 
@@ -72,10 +73,10 @@ func SetNickName(phone, nickname string) error {
 	checkErr(err)
 	_, err = stmt.Exec(nickname, phone)
 	if err != nil {
-		log.Printf("Cannot set user[%s]'s nickname[%s], for %s\n\r", phone, nickname, err.Error())
+		log.Printf("Cannot set user[%s]'s nickname[%s], for %s\n", phone, nickname, err.Error())
 		return err
 	}
-	log.Printf("Set user[%s] nickname[%s]\n\r", phone, nickname)
+	log.Printf("Set user[%s] nickname[%s]\n", phone, nickname)
 	return nil
 }
 
@@ -84,10 +85,10 @@ func UpdatePassword(phone, newPassword string) error {
 	checkErr(err)
 	_, err = stmt.Exec(newPassword, phone)
 	if err != nil {
-		log.Printf("Cannot change user[%s]'s password to new password[%s], for %s\n\r", phone, newPassword, err.Error())
+		log.Printf("Cannot change user[%s]'s password to new password[%s], for %s\n", phone, newPassword, err.Error())
 		return err
 	}
-	log.Printf("Change user[%s]'s password\n\r", newPassword)
+	log.Printf("Change user[%s]'s password\n", newPassword)
 	return nil
 }
 
@@ -101,19 +102,41 @@ func QueryUser(phone string) ([2]string, error) {
 		var nickname sql.NullString
 		err = rows.Scan(&phone, &password, &nickname)
 		if err != nil {
-			log.Printf("Find user[%s] failed\n\r, for %s", phone, err.Error())
+			log.Printf("Find user[%s] failed\n, for %s", phone, err.Error())
 			return [2]string{}, err
 		}
 		return [2]string{password, nullToEmpty(nickname)}, nil
 	}
-	log.Printf("Cannot find user[%s]\n\r", phone)
+	log.Printf("Cannot find user[%s]\n", phone)
 	return [2]string{"", ""}, nil
 }
 
-func SetKeyValue(k, v string) error {
+func SetKeyExpire(k string, expSec int) error {
+	_, err := conn.Do("Expire", k, expSec)
+	if err != nil {
+		log.Printf("Cannot set(k=%s) expire time(t=%d) on redis, for %s\n", k, expSec, err.Error())
+	}
+	return err
+}
+
+func SetKeyValue(k, v string, expSec... int) error {
 	_, err := conn.Do("set", k, v)
 	if err != nil {
-		log.Printf("Cannot set(k=%s, v=%s) on redis, for %s\n\r", k, v, err.Error())
+		log.Printf("Cannot set(k=%s, v=%s) on redis, for %s\n", k, v, err.Error())
+	}
+	if len(expSec) > 0 {
+		err = SetKeyExpire(k, expSec[0])
+	}
+	return err
+}
+
+func SetKeyByteValue(k string, v []byte, expSec... int) error {
+	_, err := conn.Do("set", k, v)
+	if err != nil {
+		log.Printf("Cannot set with byte value(k=%s, v=%v) on redis, for %s\n", k, v, err.Error())
+	}
+	if len(expSec) > 0 {
+		err = SetKeyExpire(k, expSec[0])
 	}
 	return err
 }
@@ -121,8 +144,23 @@ func SetKeyValue(k, v string) error {
 func GetKeyValue(k string) (string, error) {
 	rep, err := conn.Do("get", k)
 	if err != nil {
-		log.Printf("Connot get value of k=%s, for %s\n\r", k, err.Error())
+		log.Printf("Connot get value of k=%s, for %s\n", k, err.Error())
 		return "", err
 	}
-	return rep.(string), err
+	if rep == nil {
+		return "", err
+	}
+	return string(rep.([]uint8)), err
+}
+
+func GetKeyByteValue(k string) ([]byte, error) {
+	rep, err := conn.Do("get", k)
+	if err != nil {
+		log.Printf("Connot get value of k=%s, for %s\n", k, err.Error())
+		return nil, err
+	}
+	if rep == nil {
+		return nil, err
+	}
+	return (rep.([]uint8)), err
 }
